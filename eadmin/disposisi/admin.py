8e4fc419aa689b7f197687e50e297256befb6070
anzhari.p@gmail.com <anzhari.p@gmail.com>
2020-05-12 14:34:55 +0700

    Built baseline models

--- eadmin/disposisi/admin.py
+++ eadmin/disposisi/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
+from .models import Agency, WorkUnit, Position, Account, Attachment, MemoState, MemoType, MemoTrait, MemoCategory, Memo
+
+
 # Register your models here.
+
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'description', 'created_at', 'updated_at')
+
+
+class WorkUnitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'agency', 'description', 'created_at', 'updated_at')
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'work_unit', 'description', 'created_at', 'updated_at')
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'nip', 'position', 'work_unit', 'agency', 'created_at', 'updated_at')
+
+
+class MemoStateAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'previous_state', 'next_state', 'authorized_changer', 'description', 'created_at', 'updated_at')
+
+
+class MemoTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+
+
+class MemoTraitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+
+
+class MemoCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+
+
+class MemoAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject', 'information', 'memo_state', 'sender', 'author', 'type', 'trait', 'category', 'created_at',
+        'updated_at')
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'file', 'memo', 'created_at', 'updated_at')
+
+
+admin.site.register(Agency, AgencyAdmin)
+admin.site.register(WorkUnit, WorkUnitAdmin)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(MemoState, MemoStateAdmin)
+admin.site.register(MemoType, MemoTypeAdmin)
+admin.site.register(MemoTrait, MemoTraitAdmin)
+admin.site.register(MemoCategory, MemoCategoryAdmin)
+admin.site.register(Memo, MemoAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
--- eadmin/disposisi/models.py
+++ eadmin/disposisi/models.py
@@ -1,3 +1,121 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Agency(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WorkUnit(models.Model):
+    name = models.CharField(max_length=255)
+    agency = models.ForeignKey(Agency, on_delete=models.PROTECT)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+    work_unit = models.ForeignKey(WorkUnit, on_delete=models.PROTECT)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    nip = models.CharField(max_length=255)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+    work_unit = models.ForeignKey(WorkUnit, on_delete=models.PROTECT)
+    agency = models.ForeignKey(Agency, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MemoState(models.Model):
+    name = models.CharField(max_length=255)
+    previous_state = models.ForeignKey('self', on_delete=models.PROTECT, related_name='fk_previous_state', null=True)
+    next_state = models.ForeignKey('self', on_delete=models.PROTECT, related_name='fk_next_state', null=True)
+    authorized_changer = models.ForeignKey(User, on_delete=models.PROTECT)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MemoType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MemoTrait(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MemoCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, default='Description')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Memo(models.Model):
+    subject = models.CharField(max_length=255)
+    information = models.CharField(max_length=255)
+    memo_state = models.ForeignKey(MemoState, on_delete=models.PROTECT)
+    sender = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    type = models.ForeignKey(MemoType, on_delete=models.PROTECT)
+    trait = models.ForeignKey(MemoTrait, on_delete=models.PROTECT)
+    category = models.ForeignKey(MemoCategory, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Attachment(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    file = models.FileField(upload_to='attachment/')
+    memo = models.ForeignKey(Memo, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
